3445a664d416aca11f14c5289f5fb820d68c8658
Kapil Dhingra <kapil.dhingra@macys.com>
2015-12-09 15:32:31 -0800

    Initial Commit

---
+++ MacysAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MacysAssignment
+//
+//  Created by Kapil Dhingra on 12/9/15.
+//  Copyright © 2015 Kapil Dhingra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
